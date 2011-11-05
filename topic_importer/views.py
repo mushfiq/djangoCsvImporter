24e73b4b5739302622020a67a5b1f0cebea9ee91
Mohammed Mushfiq <mushfiq@newscred.com>
2011-11-05 23:12:25 +0600

    user can inser csv data using UI

--- topic_importer/views.py
+++ topic_importer/views.py
@@ -11,17 +11,25 @@ def index(request):
 
 
 def test(request):
-    tes_list = csv_to_list("/home/caveman/Desktop/NewsCred_Django_App/imdb.csv")
+    print settings.DIRNAME
+    tes_list = csv_to_list(settings.DIRNAME+"/topic_importer/file.csv")
     for t in tes_list:
         if(len(t) >0):
             data_insert = movie_topic(movie_name=t[0],imdb_id=t[1])
             try:
                 data_insert.save()
-                print "inserted!"
-            except e:
-                print e
+                msg =  "inserted!"
 
-    return HttpResponse("sucess!")
+            except e:
+                msg = "unable to insert :("
+               
+    return render_to_response('index.html',
+            { 'msg':msg },
+            context_instance=RequestContext(request),
+            )
+    #return HttpResponse("sucess!")
+
+    
 
 def file_upload(request):
     print settings.STATIC_ROOT
@@ -29,22 +37,18 @@ def file_upload(request):
     if request.method == 'POST':
         try:
             if(handle_uploaded_file(request.FILES['myfile']) == "true"):
-                return render_to_response('base.html',context_instance=RequestContext(request))
+                msg = "success"
+                return render_to_response('index.html',
+                        { 'msg':msg },
+                context_instance=RequestContext(request),
+                )
             else:
                  return render_to_response('error.html', context_instance=RequestContext(request))
         except Exception, e:
             print e
 
-
     return render_to_response(
         'index.html',
             {'settings.STATIC_ROOT':settings.STATIC_ROOT}, context_instance=RequestContext(request))
-        
-
-
-   
-        #print request
-
-    #return HttpResponse("Inside file upload view!")
 
 
